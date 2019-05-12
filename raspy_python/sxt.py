100e8cfecaddaaa45f197c13b3e628f4d27fc701
Zhengyc <2521230598@qq.com>
2019-05-12 14:36:13 +0800

    sxt
    sxt

--- raspy_python/sxt.py
+++ raspy_python/sxt.py
@@ -5,9 +5,10 @@ import datetime
 
 """set camera"""
 def save_pigture():
-    pic_file_path = os.path.join(CURRENT_SETTINGS.root_path,"save_picture")
+    pic_file_path = os.path.join(CURRENT_SETTINGS.root_path,"static")
     time = datetime.datetime.now()
-    name = str(time.year).zfill(4)+str(time.month).zfill(2)+str(time.day).zfill(2)+".png"
+    #name = str(time.year).zfill(4)+str(time.month).zfill(2)+str(time.day).zfill(2)+".png"
+    name = "farm_now_5.jpg"
     pic_path = os.path.join(pic_file_path,name)
     with PiCamera() as camera:
         camera.resolution = (1920,1080)
